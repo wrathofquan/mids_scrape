17ef06fca1d36c962092456733f9eff3d73f8079
Josh Quan <wrathofquan@gmail.com>
2019-10-18 11:54:49 -0700

    fix initiator variable

--- mids_clean.R
+++ mids_clean.R
@@ -13,7 +13,6 @@ webElem$sendKeysToElement(list("castro200kgd")) #authenticate
 webElem <- driver$findElement(using = "css selector", value = "input.inputbutton")
 webElem$clickElement()
 
-
 #loop to grab MIDS urls for later scraping
 counter <- (1:1484)
 df <- list()
@@ -25,38 +24,34 @@ print(temp)
 df <- rbind(df, temp) 
 }
 
-
-
 #filter out weird urls containing 'edit' in them
 df_1 <- Filter(function(x) !any(grepl("edit", x)), df)
 
-# 
+# loop through urls and scrape
 final_df <- list()
 for(i in df_1){
-  
 driver$navigate(i)
 page <- read_html(driver$getPageSource()[[1]]) %>% html_text() 
 page <- gsub(".* ARTICLE TITLE AND DATE HERE"," ", page) 
 page <- gsub("Page last modified.*", " ", page)
 page <- gsub("[\r\n\t]", "", page)
 page <- gsub("\\(Edit Section ↓)", "", page)
-page_split <- str_split(string = page, pattern = "Record of Sources Searched", simplify = TRUE)
+page_split <- str_split(string = page, pattern = "Record of Sources Searched", simplify = TRUE) # split for accuracy of fields
 ##start extracting the fields for each MID
 mid_ID <- as.numeric(gsub(".*Dispute InformationMID #: |MID Name:.* ", "", page[1]))
 url <- paste0("http://web.stanford.edu/group/tomzgroup/cgi-bin/pmwiki/index.php?n=MID.MID-0007")
 mid_name <- gsub(".*MID Name: |Start Date:.*", "", page_split[, 1])
 start_date <- gsub(".*Start Date:|End Date:.*", "", page_split[, 1])
 end_date <- gsub(".*End Date: |Initiators?:.*", "", page_split[, 1])
-initiator <- gsub(".*Initiators?: |Targets?:.*", "", page_split[, 1])## this is messed up. next try splitting entire vector into two and try again
-target <- gsub(".*Targets?: |Highest Overall Act:.*", "", page_split[, 1])##AGGG
+initiator <- gsub(".*Initiators?: |Targets?:.*", "", page_split[, 1])
+target <- gsub(".*Targets?: |Highest Overall Act:.*", "", page_split[, 1])
 highest_overall_act <- gsub(".*Highest Overall Act: |Total Fatalities:.*","", page_split[, 1])
 total_fatalities <- gsub(".*Total Fatalities: |Outcome:.*", "",page_split[, 1])
 outcome <- gsub(".*Outcome:|Location:.*","", page_split[, 1])
 location <- gsub(".*Location:|Incident:.*","", page_split[, 1])
 incidents <- gsub(".*Incident: |MID 2.1 Sources:.*", "", page_split[, 1])
 mid_sources <- gsub(".*MID 2.1 Sources: ", "", page_split[, 1])
-
-###
+### scrape second split
 search_records <- gsub(".*ProQuest Historical Newspapers:|Narrative.*","", page_split[,2])
 narrative <- gsub(".*Narrative|Non-state actor involvement?.*", "", page)
 non_state_actor_involvement <- gsub(".*Non-state actor involvement\\?|Issue Description.*","", page_split[,2])
@@ -72,16 +67,8 @@ temp_df <- as.data.frame(cbind(mid_ID, mid_name, start_date, end_date, initiator
                  mid_sources, narrative, non_state_actor_involvement, issue_description,
                  authority, disputed_facts, ultimatums_demands, notes, articles, search_records, url))
 
-final_df <- rbind(final_df, temp_df) %>% na.omit
-
-}
-
-
-
+final_df <- rbind(final_df, temp_df) %>% na.omit}
 
 write_csv(final_df, "mids_scrape.csv")
-
-
-write_file(page, "page.txt")
 
 
